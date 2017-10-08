6320e9c27753afd6d2e3cb36380d94a4ade71667
Marcin Pastwa <marcinpastwa@wp.pl>
2017-10-08 16:05:36 +0200

    ReadingFile

---
+++ 02-file-analyzer/src/main/java/com.isa.homeworks/Activity.java
@@ -0,0 +1,35 @@
+package com.isa.homeworks;
+
+public class Activity {
+    private String activityName;
+    //private String activityCategoryName;
+    private int activitySpentTime;
+    //private String activityStartDate;
+    private int activityStartHour;
+
+
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public int getActivitySpentTime() {
+        return activitySpentTime;
+    }
+
+    public void setActivitySpentTime(int activitySpentTime) {
+        this.activitySpentTime = activitySpentTime;
+    }
+
+    public int getActivityStartHour() {
+        return activityStartHour;
+    }
+
+    public void setActivityStartHour(int activityStartHour) {
+        this.activityStartHour = activityStartHour;
+    }
+}
--- 02-file-analyzer/src/main/java/com.isa.homeworks/App.java
+++ 02-file-analyzer/src/main/java/com.isa.homeworks/App.java
@@ -1,18 +1,51 @@
 package com.isa.homeworks;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-       Statistics statistics = new Statistics();
-       statistics.readFile();
 
 
+
+
+    public static void main( String[] args ) {
+
+        ArrayList<Activity> activityList = new ArrayList<>();
+        FileReader readActivitiesFile = new FileReader("activities.csv");
+        Scanner fileScanner = readActivitiesFile.getScanner();
+        Pattern pattern = Pattern.compile("([0-9]{4}-[0-9]{2}-[0-9]{2}) (.+):.+,(.+),.+,(.+),(.+),(.+)$");
+
+        int i = 0;
+        while (fileScanner.hasNextLine()) {
+            Activity activity = new Activity();
+            String activityItem = fileScanner.nextLine();
+            Matcher matcher = pattern.matcher(activityItem);
+
+            if (matcher.matches()) {
+                activity.setActivityName(matcher.group(4));
+                activity.setActivitySpentTime(Integer.valueOf(matcher.group(6)));
+                activity.setActivityStartHour(Integer.valueOf(matcher.group(2)));
+
+                activityList.add(activity);
+
+            i++;
+            }
+        }
+
+        for (Activity act: activityList) {
+
+            System.out.println(act.getActivityName());
+            System.out.println(act.getActivitySpentTime());
+            System.out.println(act.getActivityStartHour());
+        }
+
+
+        System.out.println("1) Activities: "+activityList.size());
 
     }
 }
