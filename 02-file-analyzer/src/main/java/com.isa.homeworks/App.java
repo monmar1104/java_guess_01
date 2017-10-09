0f3c922880a6615ea01c9820510fab590d1e45b7
Marcin Pastwa <marcinpastwa@wp.pl>
2017-10-09 22:36:21 +0200

    ready before objects

--- 02-file-analyzer/src/main/java/com.isa.homeworks/App.java
+++ 02-file-analyzer/src/main/java/com.isa.homeworks/App.java
@@ -33,12 +33,10 @@ public class App {
             }
         }
         int sum = 0;
-        double average;
         ArrayList<String> facebookHours = new ArrayList<>();
         ArrayList<Integer> hoursList = new ArrayList<>();
-        int[] spentTiameSum = new int[hoursList.size()];
+        ArrayList<Integer> spentTimeSum1 = new ArrayList<>();
         ArrayList<Integer> spentTimeList = new ArrayList<>();
-        int hour = 0;
         for (Activity act: activityList) {
             spentTimeList.add(act.getActivitySpentTime());
             sum = sum+act.getActivitySpentTime();
@@ -50,33 +48,25 @@ public class App {
             if(!hoursList.contains(act.getActivityStartHour())){
                 hoursList.add(act.getActivityStartHour());
             }
-            if(hour==act.getActivityStartHour()){
-                hoursList.indexOf(act.getActivityStartHour());
-                //spentTiameSum[hoursList.indexOf(act.getActivityStartHour())] = spentTiameSum[hoursList.indexOf(act.getActivityStartHour())]+act.getActivitySpentTime();
+        }
+        for (Integer hours : hoursList) {
+            int hourSum = 0;
+            for (Activity actSumSpentTime : activityList) {
+                if(hours.equals(actSumSpentTime.getActivityStartHour())){
+                    hourSum=hourSum+actSumSpentTime.getActivitySpentTime();
+                }
             }
-            else {
-                hour = act.getActivityStartHour();
-
-            }
-
+            spentTimeSum1.add(hourSum);
         }
 
-        average = (double) sum/spentTimeList.size();
         System.out.println("1) Activities: "+activityList.size());
         System.out.println("2) Statystyka wszystkich aktywności:");
         System.out.println("   a) Najkrótsze: "+Collections.min(spentTimeList));
         System.out.println("   b) Najdłuższe: "+Collections.max(spentTimeList));
-        System.out.println("   c) Średnia aktywność: "+BigDecimal.valueOf(average).setScale(2,0));
+        System.out.println("   c) Średnia aktywność: "+BigDecimal.valueOf((double) sum/spentTimeList.size()).setScale(2,0));
         System.out.println("   d) Łączny czas pracy: "+(sum/3600)+" godzin i "+((sum%3600)/60)+" minut");
         System.out.println("3) Godziny na facebooku: "+facebookHours);
-        System.out.println("4) Najbardziej intensywna godzina: "+" (Pracowano) "+" minut");
-            System.out.println(hoursList);
-
-            for (int i=0;i<spentTiameSum.length;i++){
-                System.out.println(spentTiameSum[i]);
-            }
-
-
+        System.out.println("4) Najbardziej intensywna godzina: "+hoursList.get(spentTimeSum1.indexOf(Collections.max(spentTimeSum1)))+" (Pracowano "+ Collections.max(spentTimeSum1)/60 +" minut)");
 
     }
 }
